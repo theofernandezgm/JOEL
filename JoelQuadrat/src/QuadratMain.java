ba29ef3fbbf88c46dcd7d426de0d22d9e82f64ca
theofernandezgm <theofernandezgm@gmail.com>
2024-11-18 21:45:47 +0100

    Initial commit

---
+++ JoelQuadrat/src/QuadratMain.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class QuadratMain {
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        int numero = scanner.nextInt();
+        System.out.println(numero * numero);
+    }
+}
